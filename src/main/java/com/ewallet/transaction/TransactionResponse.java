5506b64c2bf40e15992057bb5672d945f8d2351c
Shivam Agrawal <shivam19.aggrawal@gmail.com>
2026-07-04 02:09:12 +0530

    perf: add Redis caching with proper serialization

--- src/main/java/com/ewallet/transaction/TransactionResponse.java
+++ src/main/java/com/ewallet/transaction/TransactionResponse.java
@@ -1,27 +1,20 @@
 package com.ewallet.transaction;
 
 import lombok.Data;
-
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-public class TransactionResponse {
-
+public class TransactionResponse implements Serializable {
     private Long id;
-
     private Long fromUserId;
     private Long toUserId;
-
     private String senderName;
     private String senderEmail;
-
     private String receiverName;
     private String receiverEmail;
-
     private Double amount;
-
     private String type;
     private String status;
-
     private LocalDateTime createdAt;
 }
