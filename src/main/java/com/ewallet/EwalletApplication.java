a546a0e04413913100356108c8bb024c98065db0
Shivam Agrawal <shivam19.aggrawal@gmail.com>
2026-07-04 01:42:42 +0530

    Update EwalletApplication.java

--- src/main/java/com/ewallet/EwalletApplication.java
+++ src/main/java/com/ewallet/EwalletApplication.java
@@ -1,11 +1,13 @@
 package com.ewallet;
 
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 @EnableCaching
 @SpringBootApplication
 public class EwalletApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EwalletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EwalletApplication.class, args);
+    }
 }
